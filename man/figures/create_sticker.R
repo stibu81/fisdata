9a3b5c4adbe840384b82db26ae445cbe77fb13c2
stibu81 <slanz1137@gmail.com>
2025-02-09 20:11:40 +0100

    add hex sticker

---
+++ man/figures/create_sticker.R
@@ -0,0 +1,13 @@
+
+hexSticker::sticker(
+  devtools::package_file("man", "figures", "racer.png"),
+  package = "fisdata",
+  p_size = 15, p_color = "#fcaf16",
+  p_family = "sans",
+  p_x = 1.5, p_y = 0.6,
+  s_x = 1.1, s_y = 1.0, s_width = .7,
+  h_fill = "#fffbd6",
+  h_color = "#fcaf16",
+  h_size = 1.5,
+  filename = devtools::package_file("man", "figures", "fisdata_logo.png")
+)
