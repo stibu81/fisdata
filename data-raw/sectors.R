1c3d0d15873929c72666117f1a537ad0a18e6ac7
stibu81 <slanz1137@gmail.com>
2025-02-23 07:41:05 +0100

    determine dataset sectors from the page

--- data-raw/sectors.R
+++ data-raw/sectors.R
@@ -1,29 +1,21 @@
-# prepare table of sectors. This data has been manually extracted from
-# https://www.fis-ski.com/DB/general/biographies.html as follows:
-# * enter a nation that likely has athletes in each sector (e.g., SUI)
-#   into the field "Nation" to limit the number of search results
-# * select a sector from the drop down "Sector"
-# * run the search
-# * read the code for the sector from the column "Disc."
-# * repeat for all sectors from the drop down
-# the current version of the data has been extracted on 2025-01-29
+# prepare table of sectors.
 
-sectors <- tibble::tribble(
-    ~"code",       ~"description",
-    "CC",          "Cross-Country",
-    "JP",          "Ski Jumping",
-    "NK",          "Nordic Combined",
-    "AL",          "Alpine Skiing",
-    "FS",          "Freestyle",
-    "SB",          "Snowboard",
-    "SS",          "Speed Skiing",
-    "GS",          "Grass Skiing",
-    "TM",          "Telemark",
-    "MA",          "Masters",
-    "PCC",         "Para Cross-Country",
-    "PAL",         "Para Alpine Skiing",
-    "PSB",         "Para Snowboard",
-    "FR",          "Freeride"
-  )
+library(rvest)
+library(dplyr)
+library(waldo)
+
+url <- "https://www.fis-ski.com/DB/general/biographies.html"
+html <- read_html(url)
+options <- html %>%
+  html_element("select#sectorcode") %>%
+  html_elements("option")
+sectors <- tibble(
+    code = html_attr(options, "value"),
+    description = html_text(options)
+  ) %>%
+  filter(code != "")
+
+# compare with the current version of the table in the package
+compare(sectors, fisdata::sectors)
 
 usethis::use_data(sectors, overwrite = TRUE)
