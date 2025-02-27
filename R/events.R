a49c56452b0d0b66f46aff5973fb9d2db82e2b8e
stibu81 <slanz1137@gmail.com>
2025-02-27 22:12:21 +0100

    extend doc for query_events()

--- R/events.R
+++ R/events.R
@@ -1,5 +1,11 @@
 #' Query Events
 #'
+#' Query events using various filters. Omitting a filter means that events
+#' with any value in that field will be returned. In order to reduce the number
+#' of results, the FIS page automatically restricts the results to one season
+#' or even one month, if only few filters are used (see 'Details').
+#' Filtering is case-insensitive and for `place`, string matching is partial.
+#'
 #' @param selection which events should be returned: past events, where results
 #'  are available ("results") or upcoming events ("upcoming") or both ("all")?
 #' @param month numeric giving the month of the year to filter for. The month
@@ -20,8 +26,30 @@
 #' Other special characters must be replaced by the suitable
 #' substitute by the user.
 #'
+#' Results are always limited to events from a single season. If no season is
+#' explicitly provided, the current season is used. If no or few filters are
+#' applied, the results are further limited to a single month. If no month is
+#' explicitly specified, the current month is used.
+#'
 #' @returns
-#' A tibble.
+#' A tibble with the following columns: `start_date`, `end_date`, `place`,
+#' `nation`, `sector`, `categories`, `disciplines`, `genders`,
+#' `cancelled`, and `event_id`.
+#'
+#' @examples
+#' \dontrun{
+#' # query alpine skiing world cup events in February 2024
+#' query_events(sector = "AL", category = "WC", season = 2024, month = 2)
+#'
+#' # query ski jumping events on the large hill in the season 2020/21
+#' query_events(sector = "JP", discipline = "LH", season = 2021)
+#'
+#' # query cross country events on 2023-03-07
+#'
+#' # calling query_events() without any argument returns all events from the
+#' # current month
+#' query_events()
+#' }
 #'
 #' @export
 
